ea6b6766311340f730fe971282eb9961087877e1
Erwin Ramirez <er201397@gmail.com>
2023-04-21 21:00:43 -0600

    Categoria
    Implementacion del modudo competente a Categoria

--- Oracle.Services/Actions/CategoriaService.cs
+++ Oracle.Services/Actions/CategoriaService.cs
@@ -1,4 +1,5 @@
-﻿using Oracle.DataAccess.Models;
+﻿using Microsoft.EntityFrameworkCore;
+using Oracle.DataAccess.Models;
 using Oracle.Services.Interfaces;
 using System;
 using System.Collections.Generic;
@@ -17,29 +18,101 @@ namespace Oracle.Services.Actions
             _context = contect;
         }
 
-        public Task<RespuestaService<CategoriaProducto>> Actualizar(CategoriaProducto ct)
+        public async Task<RespuestaService<CategoriaProducto>> Actualizar(CategoriaProducto ct)
         {
-            throw new NotImplementedException();
+            var resp = new RespuestaService<CategoriaProducto>();
+            var cate = await _context.CategoriaProductos.FirstOrDefaultAsync(x => x.Idcategoriaproducto == ct.Idcategoriaproducto);
+
+            if (cate == null)
+                resp.AgregarBadRequest("ID recibido no esta registrado");
+            else
+                cate.Nombre = ct.Nombre;
+                cate.Tipomueble = ct.Tipomueble;
+                cate.Descripcion = ct.Descripcion;
+            try
+            {
+                _context.CategoriaProductos.Update(cate);
+                await _context.SaveChangesAsync();
+
+                resp.Objeto = cate;
+            }
+            catch (DbUpdateException ex)
+            {
+                resp.AgregarInternalServerError(ex.Message);
+            }
+
+            return resp;
         }
 
-        public Task<RespuestaService<CategoriaProducto>> BuscarPorId(int id)
+        public async Task<RespuestaService<CategoriaProducto>> BuscarPorId(int id)
         {
-            throw new NotImplementedException();
+            var resp = new RespuestaService<CategoriaProducto>();
+            var cate = await _context.CategoriaProductos.FirstOrDefaultAsync(x => x.Idcategoriaproducto == id);
+
+            // valida la existencia del ID del usuario
+            if (cate == null)
+                resp.AgregarBadRequest("ID ingresado no esta registrado");
+            else
+                resp.Objeto = cate;
+            return resp;
         }
 
-        public Task<RespuestaService<bool>> Eliminar(int id)
+        public async Task<RespuestaService<bool>> Eliminar(int id)
         {
-            throw new NotImplementedException();
+            var resp = new RespuestaService<bool>();
+            var cate = await _context.CategoriaProductos.FirstOrDefaultAsync(x => x.Idcategoriaproducto == id);
+
+            if (cate == null)
+                resp.AgregarBadRequest("ID ingresado no esta registrado");
+            else
+            {
+                try
+                {
+                    _context.CategoriaProductos.Remove(cate);
+                    await _context.SaveChangesAsync();
+                    resp.Exito = true;
+                }
+                catch (DbUpdateException ex)
+                {
+                    resp.AgregarInternalServerError(ex.Message);
+                }
+            }
+
+            return resp;
         }
 
-        public Task<RespuestaService<CategoriaProducto>> Guardar(CategoriaProducto ct)
+        public async Task<RespuestaService<CategoriaProducto>> Guardar(CategoriaProducto ct)
         {
-            throw new NotImplementedException();
+            var resp = new RespuestaService<CategoriaProducto>();
+
+            try
+            {
+                await _context.CategoriaProductos.AddAsync(ct);
+                await _context.SaveChangesAsync();
+                ct.Idcategoriaproducto = await _context.Proveedors.MaxAsync(cate => cate.Idproveedor);
+
+                resp.Objeto = ct;
+            }
+            catch (DbUpdateException ex)
+            {
+                resp.AgregarBadRequest(ex.Message);
+            }
+
+            return resp;
         }
 
-        public Task<RespuestaService<List<CategoriaProducto>>> Listar()
+        public async Task<RespuestaService<List<CategoriaProducto>>> Listar()
         {
-            throw new NotImplementedException();
+            //implementa y despliega el resultado de la lista 
+            var resp = new RespuestaService<List<CategoriaProducto>>();
+            var lista = await _context.CategoriaProductos.ToListAsync();
+
+            if (lista != null)
+                resp.Objeto = lista;
+            else
+                resp.AgregarInternalServerError("Se encontro un Error");
+
+            return resp;
         }
     }
 }
---
+++ Oracle.WebApi/Controllers/CategoriaController.cs
@@ -0,0 +1,102 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Oracle.DTO;
+using Oracle.Services.Interfaces;
+using Oracle.WebApi.Mappings;
+
+namespace Oracle.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriaController : ControllerBase
+    {
+        private readonly ICategoriaService _servicio;
+
+        public CategoriaController(ICategoriaService servicio)
+        {
+
+            _servicio = servicio;
+        }
+
+        // Lista la informacion de los Categoria
+        [HttpGet]
+        public async Task<ActionResult<List<CategoriaDTO>>> Listar()
+        {
+
+            //Utilizado por el servicio creado ICategoriaService
+
+            var retorno = await _servicio.Listar();
+
+            //validacion del servicio
+            if (retorno.Objeto != null)
+                // return retorno.Objeto; Sin aplicar el uso del servicio
+                return retorno.Objeto.Select(MapperCat.ToDTO).ToList();
+            else
+                return StatusCode(retorno.Status, retorno.Error);
+
+
+        }
+
+
+        // Lista la informacion de los Categorias segun la ID ingresado
+        [HttpGet("{id}")]
+        public async Task<ActionResult<CategoriaDTO>> BuscarPorId(int id)
+        {
+
+
+            var retorno = await _servicio.BuscarPorId(id);
+
+            //validacion del servicio
+            if (retorno.Objeto != null)
+                return retorno.Objeto.ToDTO();
+            else
+                return StatusCode(retorno.Status, retorno.Error);
+
+        }
+
+
+        // Ingresa de datos
+        [HttpPost]
+        public async Task<ActionResult<CategoriaDTO>> Guardar(CategoriaDTO ct)
+        {
+
+
+            var retorno = await _servicio.Guardar(ct.ToDatabase());
+
+            if (retorno.Objeto != null)
+                return retorno.Objeto.ToDTO();
+            else
+                return StatusCode(retorno.Status, retorno.Error);
+
+        }
+
+
+        // Actualiza datos
+        [HttpPut]
+        public async Task<ActionResult<CategoriaDTO>> Actualizar(CategoriaDTO ct)
+        {
+
+
+            var retorno = await _servicio.Actualizar(ct.ToDatabase());
+
+            if (retorno.Objeto != null)
+                return retorno.Objeto.ToDTO();
+            else
+                return StatusCode(retorno.Status, retorno.Error);
+        }
+
+
+        // Elimina segun el ID ingresado
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<bool>> Eliminar(int id)
+        {
+
+            var retorno = await _servicio.Eliminar(id);
+
+            if (retorno.Exito)
+                return true;
+            else
+                return StatusCode(retorno.Status, retorno.Error);
+        }
+    }
+}
